ad74bfce8d514509fb2cadaf5de99460b2bd5dda
__Hxk__ <ga197702@gmail.com>
2024-03-15 10:24:40 -0500

    NewProyect

---
+++ ScheduleMaster/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScheduleMaster
+//
+//  Created by Innov8ive on 3/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Buenos Dias")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScheduleMaster/ScheduleMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScheduleMasterApp.swift
+//  ScheduleMaster
+//
+//  Created by 64026339 on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ScheduleMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
