7f6f4b05724027c261eed81c4424f37a485b6732
__Hxk__ <ga197702@gmail.com>
2024-03-15 15:17:58 -0500

    Color, and Date chooser in Calender file, + file of configuration for calendar

---
+++ ScheduleMaster/CalendarFiles/Date+Extention.swift
@@ -0,0 +1,32 @@
+//
+//  Date+Extention.swift
+//  ScheduleMaster
+//
+//  Created by 64023073 on 3/15/24.
+//
+
+import Foundation
+
+extension Date {
+    //https://gist.github.com/StewartLynch/90ecac7eff9fe438bba580e96643fe70
+    static var capitalizedFirstLettersOfWeekdays: [String] {
+        let calendar = Calendar.current
+        let weekdays = calendar.shortWeekdaySymbols
+
+        return weekdays.map { weekday in
+            guard let firstLetter = weekday.first else { return "" }
+            return String(firstLetter).capitalized
+        }
+    }
+    
+    static var fullMonthNames: [String] {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale.current
+
+        return (1...12).compactMap { month in
+            dateFormatter.setLocalizedDateFormatFromTemplate("MMMM")
+            let date = Calendar.current.date(from: DateComponents(year: 2000, month: month, day: 1))
+            return date.map { dateFormatter.string(from: $0) }
+        }
+    }
+}
